4dc2e40fe77ae0322808cdb94ced8fbfbc109298
Matt Ferrante <mferrante3@gmail.com>
2014-01-30 23:51:31 -0500

    added back the start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+DIR="logs"
+START=`date '+%Y_%m_%d'`
+LOG_FILE="$DIR/log_$START.log"
+mkdir $DIR 2>/dev/null
+echo "Logging to: #$LOG_FILE";
+node retweet.js >> $LOG_FILE &
+echo "Started..."
+ps ax | grep node
